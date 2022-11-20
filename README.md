62ac8879ee5326c00b3533e6d5e16bf0bdd9f806
Darren Shaw <darrenaustinshaw@gmail.com>
2022-11-20 09:39:53 -0700

    two-sum

--- README.md
+++ README.md
@@ -6,6 +6,7 @@
 ##### Array & Hashing
 
 * contains-duplicate (easy): array, hashing
+* two-sum (easy): array, addition
 * top-k-frequent-elements (medium): bucket sort, bubble sort, array, hashing
 * encode-and-decode-strings (medium): array, hashing, pretty easy
 * *longest-consecutive-sequence (medium/hard): array, hashing, O(n) time constraint, tricky to start then easy
--- arrays_hashing.py
+++ arrays_hashing.py
@@ -188,6 +188,25 @@ class Solution:
 
         return answer
 
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        """
+        [2,7,11,15,3] target = 10
+
+        [1,4]
+
+        brute force
+        nested for loop
+
+        time complexity = O(n**2)
+        space complexity = O(1)
+        """
+        l_nums = len(nums)
+
+        for i in range(l_nums):
+            for j in range(i+1,l_nums):
+                if nums[i] + nums[j] == target:
+                    return [i,j]
+
 
 class Codec:
     """
