e41842c64901a268bb6fce370b1ccd66f833ac12
Darren Shaw <darrenaustinshaw@gmail.com>
2022-11-26 14:48:38 -0700

    updating time complexity

--- neetcode_150/backtracking.py
+++ neetcode_150/backtracking.py
@@ -74,3 +74,48 @@ class Solution:
         dfs(current_solution,0)
 
         return output
+
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        """
+        dfs
+        each node you can add one from candidates
+        if val > target, stop dfs
+        if val == target, add to output if sorted(soln) not already
+
+        time complexity:
+        O(N^(T/M)) T/M essentially trying to swag on avg how much the trees will grow out
+
+        space complexity: O(T/M) T/M recursive calls
+
+        """
+
+        output = []
+        candidates = sorted(candidates) # O(log n)
+        explored_dict = {}
+
+        def dfs(soln, candidates):
+            soln_sum = sum(soln)
+            explored_dict[str(sorted(soln))] = 1
+
+            if soln_sum == target and sorted(soln) not in output:
+                # potentially add to output
+                # sorted is O(logn)
+                output.append(sorted(soln))
+
+            elif soln_sum > target:
+                return -1
+
+            else:
+                # dfs potential solution
+                for i in range(len(candidates)):
+                    new_soln = sorted(soln+[candidates[i]])
+                    if str(new_soln) not in explored_dict:
+                        feedback = dfs(new_soln, candidates[i:])
+                        if feedback == -1:
+                            break
+
+
+
+        dfs([], candidates)
+
+        return output
